ae6b3dac1cff241f7fb071c89c517abc263be9c9
Avner <avner.conceicao@hotmail.com>
2018-03-14 00:50:35 +0000

    Inicializando repositorio

---
+++ src/store/reducer/Person.js
@@ -0,0 +1,22 @@
+import { ADD_PERSON, DELETE_PERSON } from '../actions';
+
+const initialState = {
+    persons: []
+};
+
+const reducerPerson = (state = initialState, action) => {
+    switch(action.type) {
+        case ADD_PERSON:
+            return {
+                ...state,
+                persons: state.persons.concat(action.newPerson)
+            }            
+        case DELETE_PERSON:
+            return {
+                ...state,
+                persons: state.persons.filter(person => person.id !== action.personId)
+            }
+    }
+};
+
+export default reducerPerson;
