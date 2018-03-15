13452c86b1a8bf137d7dbc13bee5314122680ce8
Avner <avner@barramansa.unimed.com.br>
2018-03-15 14:44:00 -0300

    Add REDUX_DEVTOOLS

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import reducerPerson from './store/reducer/Person';
-
-const reducer = combineReducers({
-    reducerPerson
-});
+import reducer from './store/reducer/Person';
 
 //const store = createStore(reducer);
-const store = createStore(
-    reducer, /* preloadedState, */
- +  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware()
+  ));
 
 ReactDOM.render(
 <Provider store={store}>
